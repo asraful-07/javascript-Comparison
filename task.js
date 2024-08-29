380424e42e10cc422d1528759ab5642ee99e4823
asraful-07 <asraful0508@gmail.com>
2024-08-29 11:00:22 +0600

    javascript solve programimg hero task

---
+++ task.js
@@ -0,0 +1,27 @@
+// variable task 
+
+// 1st
+var orangePrice = 1000;
+var applePrice  = 700;
+
+var total = (orangePrice - applePrice);
+console.log(total)
+
+// 2nd
+var num1 =75.25;
+var num3 =65;
+var num4 =80;
+var num5 =35.45;
+var num7 =99.50;
+
+var total = (num1 + num3 + num4 + num5 + num7) / 5;
+console.log(total. toFixed(2))
+
+
+// 3rd
+var num1 = 119;
+var num2 = 5;
+
+var total = (num1 % num2);
+console.log(total);
+
--- var.js
+++ var.js
@@ -4,8 +4,12 @@
 // number
 var weight = 44;
 var price = 34;
-price = 43;
+price -= 43;
 console.log(price);
+/* 
+var pc = 30000;
+pc = pc + 10000;
+console.log(pc)*/
 
 // string
 var name = "sadar uddin"
@@ -16,7 +20,14 @@ var isRich = true;
 var passed = false;
 console.log(typeof passed)
 
-var orangePrice = 30;
+var orangePrice = 40;
 var applePrice = '30';
 console.log(orangePrice - applePrice)
+
+var orangePrice = parseInt ('30.12');
+console.log(orangePrice)
+
+var num1 = 5;
+var num2 = 2;
+console.log(num1 % num2);
 
