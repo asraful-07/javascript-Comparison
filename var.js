5e4a6e5e15da2c43cb053e199f910cfa8aee1fca
asraful-07 <asraful0508@gmail.com>
2024-08-29 09:27:17 +0600

    var js code

---
+++ var.js
@@ -0,0 +1,22 @@
+// variable
+// variable ree diclar kora jai 
+
+// number
+var weight = 44;
+var price = 34;
+price = 43;
+console.log(price);
+
+// string
+var name = "sadar uddin"
+console.log(typeof name)
+
+// boolean 
+var isRich = true;
+var passed = false;
+console.log(typeof passed)
+
+var orangePrice = 30;
+var applePrice = '30';
+console.log(orangePrice - applePrice)
+
