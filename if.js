984f7af008115e7f40acc794f43f46783e11f276
asraful-07 <asraful0508@gmail.com>
2024-09-01 11:04:25 +0600

    solve this problems

--- if.js
+++ if.js
@@ -12,3 +12,21 @@ if(isRaining === true){
 else{
     console.log('okay');
 }
+
+// 2nd 
+const brotherAge = 24;
+const myAge = 21;
+const isBrother = true;
+
+if(!!isBrother){
+    console.log('they are brother');
+ if(myAge >= brotherAge){
+    console.log('im big brother');
+ }
+ else if(brotherAge >= myAge){
+    console.log("my brother is big brother");
+ }
+}
+else{
+    console.log("they are no brother");
+}
